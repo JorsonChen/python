0b67955c7cec47f1384b01314db9eacda1406377
CYW <435178397@qq.com>
2015-12-31 17:37:08 +0800

    judge name's length

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello,python")
+print("please input yourname~")
+name = input()
+print("yourname is:"+name)
+lenght = len(name) 
+if lenght>15:
+	print('yourname is too long!')
+elif lenght>=8:
+	print('yourname is good!')
+elif lenght<4:
+	print('yourname is bad!')
