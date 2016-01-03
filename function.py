c2f9dafc0df09e801e0978a517fbd328c3288002
JorsonChen <435178397@qq.com>
2016-01-03 13:42:15 +0800

    define a function

--- function.py
+++ function.py
@@ -1,3 +1,25 @@
-r = 4 ;
-S = str(3.14 * r * r) ;
-print(S)
+#系统函数
+# r = 4 ;
+# S = str(3.14 * r * r) ;
+# print(S)
+
+
+
+#自定义函数
+# def my_abs(x):
+# 	if x >= 0:
+# 		return x
+# 	else:
+# 		return -x
+
+# total = 30
+# print(my_abs(total))
+
+import math
+def move(x,y,step,angle=0):
+	nx = x + step * math.cos(angle)
+	ny = y - step * math.sin(angle)
+	return nx,ny
+
+r = move(100,100,60,math.pi/6)
+print(r)
