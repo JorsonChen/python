2033a0b2134c1c5cf39238c755c905babaa3273f
JorsonChen <435178397@qq.com>
2016-01-04 23:10:01 +0800

    study slice

--- slice.py
+++ slice.py
@@ -1,5 +1,5 @@
 #切片
-names = ['cyw','cyz','ccy','ccc','cbd']
+names = ['cyw','cyz','ccy','ccc','cbd','cry','bde']
 #从索引0开始取，直到索引3为止
 #print(names[0:3])
 #print(names[:3])、
@@ -8,4 +8,13 @@ names = ['cyw','cyz','ccy','ccc','cbd']
 #print(names[1:3])
 
 #倒数切片（倒数第一个元素的索引是-1）
-print(names[-3])
+#print(names[-3])
+
+#某一段切片，前2个
+#print(names[:2])
+
+#某一段切片，后2个
+#print(names[-2:])
+
+#前5个，每两个取一个
+print(names[:5:2])
