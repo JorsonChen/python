720080bb3bd4f76a6e84469cb0244b020b6a8b6a
CYW <435178397@qq.com>
2016-01-04 18:40:03 +0800

    add slice.py file

---
+++ slice.py
@@ -0,0 +1,11 @@
+#切片
+names = ['cyw','cyz','ccy','ccc','cbd']
+#从索引0开始取，直到索引3为止
+#print(names[0:3])
+#print(names[:3])、
+
+#从索引1开始，取出2个元素出来
+#print(names[1:3])
+
+#倒数切片（倒数第一个元素的索引是-1）
+print(names[-3])
