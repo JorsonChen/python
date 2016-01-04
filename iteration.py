42e8ba4ad6ff5730faf120d7b65a13cdb5e8290a
JorsonChen <435178397@qq.com>
2016-01-04 23:28:20 +0800

    add iteration.py file

---
+++ iteration.py
@@ -0,0 +1,13 @@
+#迭代(in方式)
+# d = {'a':1,'b':2,'c':3}
+# for key in d:
+# 	print (key)
+
+#Python内置的enumerate函数可以把一个list变成索引-元素对
+# for i, value in enumerate(['A', 'B', 'C']):
+# 	print (i,value)
+
+# for x, y in [(1, 1), (2, 4), (3, 9)]:
+# 	print (x, y)
+
+
