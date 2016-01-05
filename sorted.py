f788cec2d2a25414bc6b3b4a4aad1a6a854b09f5
CYW <435178397@qq.com>
2016-01-05 15:20:14 +0800

    append sorted.py

--- sorted.py
+++ sorted.py
@@ -4,12 +4,25 @@
 # sl = sorted(l)
 # print(sl)
 
+
 #加入key值的排序(此时sorted是高阶函数)
 # l = [31,58,-14,29,0,-3,105]
 # sl = sorted(l,key=abs)
 # print(sl)
 
+
 #对字符串进行排序(按照ASCII大小排)
+# l = ['hello','Hi','zoo','chen','~','nishishui']
+# sl = sorted(l)
+# print(sl)
+
+#对字符串进行排序，忽略大小写(按照ASCII大小排)
+# l = ['hello','Hi','zoo','chen','~','nishishui']
+# sl = sorted(l,key=str.lower)
+# print(sl)
+
+
+#对字符串进行排序，忽略大小写，并且反向(按照ASCII大小排)
 l = ['hello','Hi','zoo','chen','~','nishishui']
-sl = sorted(l)
+sl = sorted(l,key=str.lower,reverse=True)
 print(sl)
