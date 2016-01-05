87d1c2d73b9855a2d562726378b6e9bd48dd3076
JorsonChen <435178397@qq.com>
2016-01-05 23:43:52 +0800

    增加匿名函数

--- function.py
+++ function.py
@@ -50,3 +50,26 @@
 # 	return fact_iter(num-1,num*product)
 
 # print(fact(5))
+
+
+#返回函数
+# def count():
+# 	def f(j):
+# 		def g():
+# 			return j*j
+# 		return g
+# 	fs = []
+# 	for i in range(1,5):
+# 		fs.append(f(i))
+# 	return fs
+# f1,f2,f3,f4 = count()
+# print(f1())
+# print(f2())
+# print(f3())
+# print(f4())
+
+
+#匿名函数 lambda
+# f = lambda x: x * x
+# print(f)
+# print(f(5))
