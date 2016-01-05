5d22273ed2c35419f536540098af984251595191
CYW <435178397@qq.com>
2016-01-05 15:03:45 +0800

    add sorted.py file

---
+++ sorted.py
@@ -0,0 +1,15 @@
+#排序算法
+#普通排序
+# l = [31,58,-14,29,0,-3,105]
+# sl = sorted(l)
+# print(sl)
+
+#加入key值的排序(此时sorted是高阶函数)
+# l = [31,58,-14,29,0,-3,105]
+# sl = sorted(l,key=abs)
+# print(sl)
+
+#对字符串进行排序(按照ASCII大小排)
+l = ['hello','Hi','zoo','chen','~','nishishui']
+sl = sorted(l)
+print(sl)
