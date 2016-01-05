47a44f5f8d25ad6b9a4789a5ac28aa3fee594caf
CYW <435178397@qq.com>
2016-01-05 14:44:06 +0800

    add functions

--- iteration.py
+++ iteration.py
@@ -20,11 +20,15 @@
 # print(l)
 
 #生成器
-g = (x * x for x in range(10))
-print (g)
-i = 0
-while i<10:
-	print(next(g))
-	i = i + 1
+# g = (x * x for x in range(10))
+# print (g)
+# i = 0
+# while i<10:
+# 	print(next(g))
+# 	i = i + 1
 
+#高阶函数(一个函数可以接收另外一个函数)
+def add(x,y,f):
+	return f(x)+f(y)
+print(add(-1,6,abs))
 
