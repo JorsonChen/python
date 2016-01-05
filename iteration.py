67f4f566aaa00ca17b99582291e1c461789df3a7
CYW <435178397@qq.com>
2016-01-05 10:52:06 +0800

    add a generator

--- iteration.py
+++ iteration.py
@@ -10,4 +10,21 @@
 # for x, y in [(1, 1), (2, 4), (3, 9)]:
 # 	print (x, y)
 
+#生成列表
+# l = [x * x for x in range(1,11) if x % 2 == 0]
+# print(l)
+
+#两个变量生成list
+# d = {'x':'A','y':'B','z':'C'}
+# l = [k + '=' + v for k,v in d.iteritems()]
+# print(l)
+
+#生成器
+g = (x * x for x in range(10))
+print (g)
+i = 0
+while i<10:
+	print(next(g))
+	i = i + 1
+
 
